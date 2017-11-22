80447e3cd32310c1a492c591f20e0be695989b02
Roni Fultheim <ronif10@gmai.com>
2017-11-22 13:10:28 +0200

    finished going over classes together

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,7 @@ int main() {
 	// allocate menu on stack
 	Menu menuReversi;
 
-<<<<<<< HEAD
-=======
 	// allocate logic and view on stack - currently there is only one type of logic and view, no need to allocate dynamically
->>>>>>> df317c036cd06ce269df8573170cf06780a94d63
 	ViewByConsole view;
 	StandardMoveLogic ml;
 
