abffab8be7b402fe89ae4be5c4458349718ea1d1
Roni Fultheim <ronif10@gmai.com>
2017-11-26 13:26:33 +0200

    still debugging

--- src/ComputerPlayer.cpp
+++ src/ComputerPlayer.cpp
@@ -20,17 +20,10 @@ Location ComputerPlayer::getNextMove(const ViewGame* view, const MoveLogic* logi
 	//initialize best move to fist possible move - will make sure move returned is legal at any case
 	Location bestMove(this->getPossibleMoves()[0]);
 
-	// if the opp player have no possible moves, it will return the first possible move.
-	//TODO - not sure this is needed, and quite sure it presents a logical problem: consider a situation where other player has no
-	//moves, but making a move will create moving options for opponent
-	/*if(!other->hasPossibleMoves()){
-		return  possiblCurrenteMoves[0];
-	}*/
-
 	//execute algorithm
 	//for each of the computer player possible move - check what is the highest possible score for opponent
 	for(vector<Location>::const_iterator compMove = this->getPossibleMoves().begin();
-			compMove != this->getPossibleMoves().end(); ++compMove){
+			compMove != this->getPossibleMoves().end(); compMove++){
 
 		std::cout << "loop1 for " << *compMove << "\n";
 		//copy all participating objects - to work on copies (leave originals untouched)
@@ -44,20 +37,22 @@ Location ComputerPlayer::getNextMove(const ViewGame* view, const MoveLogic* logi
 		 */
 
 		//play move for the current player
-		logic-> updateMoveOptions(copyThis, copyBoard); //TODO - should not have to do this
 		logic-> playMove(*compMove, copyThis, copyBoard, copyOther);
+
 		std::cout << "\tboard after move \n";
 		view->printBoard(copyBoard.getBoard(), 8);
 
 		//update the possible moves for the opponent player
 		logic-> updateMoveOptions(copyOther, copyBoard);   //TODO - should work
+		std::cout << "\tloop1:\n\t\tother possible moves after update: ";
+		view->messagePossibleMoves(copyOther->getPossibleMoves());
 
 		//initialize maximal seen score of a player's move to smallest number possible
 		int maxScore = numeric_limits<int>::min();
 
 		//for each of the possible moves of the other player - check what is the possible score of move
-		for(vector<Location>::const_iterator oppMove = copyOther->getPossibleMoves().begin();
-				oppMove != copyOther->getPossibleMoves().end(); ++oppMove){
+		/*for(vector<Location>::const_iterator oppMove = copyOther->getPossibleMoves().begin();
+				oppMove != copyOther->getPossibleMoves().end(); oppMove++){
 			std::cout << "\t\tloop2 for " << *oppMove << "\n";
 
 			//copy all participating objects - to work on copies (leave originals copies as changed in first loop)
@@ -65,25 +60,62 @@ Location ComputerPlayer::getNextMove(const ViewGame* view, const MoveLogic* logi
 			Player* secondCopyOther = copyOther->clone();
 			Player* secondCopyThis = copyThis->clone();
 
-			logic-> updateMoveOptions(secondCopyOther, secondCopyBoard); //TODO - should not have to do this
-			std::cout << "\tupdated opponent's options in loop2 ";
-			view->messagePossibleMoves(secondCopyOther->getPossibleMoves());
+
+			std::cout << "\tclone#2:\n\t\tother possible moves: ";
+			view->messagePossibleMoves(copyOther->getPossibleMoves());
+			std::cout << "\n\t\tthis possible moves: ";
+			view->messagePossibleMoves(copyThis->getPossibleMoves());
 			std::cout<< "\n";
+
+			logic-> updateMoveOptions(secondCopyOther, secondCopyBoard); //TODO - should not have to do this
+
 			//play the possible move for opponent, current player is now the "other player"
 			logic-> playMove(*oppMove, secondCopyOther, secondCopyBoard, secondCopyThis);
 
-			std::cout << "\tboard after SECOND move \n";
-			view->printBoard(secondCopyBoard.getBoard(), 8);
+			//std::cout << "\tboard after SECOND move \n";
+			//TODO view->printBoard(secondCopyBoard.getBoard(), 8);
 
 			//get change in scores (opponent's score minus computer's score)
 			int diff = secondCopyOther->getScore() - secondCopyThis->getScore();
-			std::cout << "\t\t\tdiff:  " << diff << "\n";
+			//std::cout << "\t\t\tdiff:  " << diff << "\n";
 
 
 			//if we found a new best move for opponent - save score
 			if (diff > maxScore) {
 				maxScore = diff;
-			}
+			}*/
+
+
+		for(int i=0; i<copyOther->getPossibleMoves().size(); i++){
+					std::cout << "\t\tloop2 for " << copyOther->getPossibleMoves()[i] << "\n";
+
+					//copy all participating objects - to work on copies (leave originals copies as changed in first loop)
+					Board secondCopyBoard(copyBoard);
+					Player* secondCopyOther = copyOther->clone();
+					Player* secondCopyThis = copyThis->clone();
+
+
+					std::cout << "\tclone#2:\n\t\tother possible moves: ";
+					view->messagePossibleMoves(copyOther->getPossibleMoves());
+					std::cout << "\n\t\tthis possible moves: ";
+					view->messagePossibleMoves(copyThis->getPossibleMoves());
+					std::cout<< "\n";
+
+					//play the possible move for opponent, current player is now the "other player"
+					logic-> playMove(copyOther->getPossibleMoves()[i], secondCopyOther, secondCopyBoard, secondCopyThis);
+
+					//std::cout << "\tboard after SECOND move \n";
+					//TODO view->printBoard(secondCopyBoard.getBoard(), 8);
+
+					//get change in scores (opponent's score minus computer's score)
+					int diff = secondCopyOther->getScore() - secondCopyThis->getScore();
+					//std::cout << "\t\t\tdiff:  " << diff << "\n";
+
+
+					//if we found a new best move for opponent - save score
+					if (diff > maxScore) {
+						maxScore = diff;
+					}
 		}
 
 		//if best move opponent can play is worse then found best move for opponent -
