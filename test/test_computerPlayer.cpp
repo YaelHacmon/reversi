b23f568d257d086d394bc2daa5fe2b2f08e7c864
Roni Fultheim <ronif10@gmai.com>
2017-11-27 12:23:09 +0200

    changed computerPlayer's test to expect single location at all tests

--- test/test_computerPlayer.cpp
+++ test/test_computerPlayer.cpp
@@ -21,13 +21,16 @@ TEST_F(ComputerPlayerTest, CloningChecks) {
 //tests getNextMove minimax algorithm
 TEST_F(ComputerPlayerTest, AlgorithmChecks) {
 	//case 1: test for first move - should be (3,3) or (5,3)
-	EXPECT_TRUE((firstMove==Location(3,3)) || (firstMove==Location(5,3)));
+	//our algorithm returns first move found - (3,3)
+	EXPECT_EQ(firstMove, Location(3,3));
 
 	//case 2: test for making the better move when there are other worse possibilities - should be either (1,5), (5,3), or (5,5)
-	EXPECT_TRUE((betterMove==Location(1,5)) || (betterMove==Location(5,3)) || (betterMove==Location(5,5)));
+	//our algorithm returns first move found - (1,5)
+	EXPECT_EQ(betterMove, Location(1,5));
 
 	//case 3: test for least worst move, when there are no good choices - should be (4,7) or (7,4)
-	EXPECT_TRUE((leastWorstMove==Location(4,7)) || (leastWorstMove==Location(7,4)));
+	//our algorithm returns first move found - (4,7)
+	EXPECT_EQ(leastWorstMove, Location(4,7));
 
 	//case 4: test making the winning move - even though there are "better scoring" moves, move should be (0,4)
 	EXPECT_EQ(winMove, Location(0,4));
