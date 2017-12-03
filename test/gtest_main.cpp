345312def830a5a64d7ede57eb2a111bc0b9ad16
Roni Fultheim <ronif10@gmai.com>
2017-12-03 18:33:33 +0200

    put testing main in a comment

--- test/gtest_main.cpp
+++ test/gtest_main.cpp
@@ -4,10 +4,11 @@
  *  Created on: Nov 15, 2017
  *      Author: roni
  */
-
+/*
 #include "../gtest_src/gtest/gtest.h"
 
 GTEST_API_ int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+*/
