daec5942bddbc192a7e001aaaff51801ded63928
Roni Fultheim <ronif10@gmai.com>
2017-12-03 21:02:54 +0200

    updated Menu to allow for creating both Player and GameManager dynamically by user's choice

--- include/Menu.h
+++ include/Menu.h
@@ -11,25 +11,48 @@
  * Allows use of the Factory design pattern for runtime creation of object types by the user's choice (Menu is the factory class).
  */
 class Menu {
-	public:
-		/**
-		 * Constructor, no parameters.
-		 * Will initialize the options string array with messages for every index
-		 */
-		Menu();
+public:
+	/**
+	 * Constructor, no parameters.
+	 * Will initialize the options string array with messages for every index
+	 */
+	Menu();
 
-		/**
-		 * Returns a type of player to play in game, by the player's choice
-		 * Uses the view to show the menu, then accepts a player's decision and acts upon it.
-		 *
-		 * @param view to show menu options with
-		 * @return a type of Player
-		 */
-		Player* getPlayerByUserChoice(ViewGame& view);
+	/**
+	 * Show the menu and accepts user's choice (then saves it).
+	 * Uses the view to show the menu, then accepts a player's decision and acts upon it.
+	 *
+	 * @param view to show menu options with
+	 */
+	void show(ViewGame& view);
 
-	private:
-		//vector to hold messages of options at index of choice. Using a vector for easy access to length along with easy iteration.
-		std::vector<std::string> playerOptions_;
+	/**
+	 * Returns a type of player to play in game (human vs. AI), by the player's choice.
+	 * Player's choice is saved from last showing of menu.
+	 *
+	 * @return a type of Player
+	 */
+	Player* getPlayerByUserChoice();
+
+	/**
+	 * Returns a type of game manager to manage the game (local vs. remote), by the player's choice.
+	 * Player's choice is saved from last showing of menu.
+	 * Must accept all arguments to allow creating the proper Game Manager.
+	 *
+	 * @param b board of game
+	 * @param p1 black player
+	 * @param p2 white player
+	 * @param ml logic to handle moves
+	 * @return a type of Game Manager
+	 */
+	GameManager* getGameManagerByUserChoice(ViewGame* v, Board& b, Player* p1, Player* p2, MoveLogic* ml);
+
+private:
+	//choice of user in last showing of menu
+	int choice_;
+
+	//vector to hold messages of options at index of choice. Using a vector for easy access to length along with easy iteration.
+	std::vector<std::string> playerOptions_;
 };
 
 
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -9,25 +9,45 @@ Menu::Menu() {
 	playerOptions_.push_back("Choose white player's type:"); //index 0
 	playerOptions_.push_back("play against a human player"); //index 1
 	playerOptions_.push_back("play against an AI player"); //index 2
+	playerOptions_.push_back("play against a remote player"); //index 3
+
+	//intialize choice to -1, to stop from getting a choice before presenting menu
 }
 
-
-Player* Menu::getPlayerByUserChoice(ViewGame& view) {
+void Menu::show(ViewGame& view) {
 	//call view to show menu and return player's option
-	//TODO - not actually a nice way to do this... maybe we should split into "show" and "get choice" - one action per method
 	int choice = view.presentMenu(playerOptions_);
+}
 
-	//create and return a player type according to user's choice TODO - should return by reference or as pointer?
-	switch(choice) {
-		case 1:
-			return new HumanPlayer("O", Board::WHITE);
+Player* Menu::getPlayerByUserChoice() {
+	//create and return a player type according to user's choice
+	switch(choice_) {
+	//in both cases 1,3 we use a human player as the opponent. When playing vs. a remote player the human
+	//player acts as a "empty" player for playing the moves received from the ooponent via the server
+	case 1:
+	case 3:
+		return new HumanPlayer("O", Board::WHITE);
 
-		case 2:
-			return new ComputerPlayer("O", Board::WHITE);
+	case 2:
+		return new ComputerPlayer("O", Board::WHITE);
 
-		default:
-			view.showMessage("Problem creating players");
-			break;
+	default:
+		throw("Problem creating players");
 	}
-	return new HumanPlayer("O",  Board::WHITE);
+}
+
+GameManager* Menu::getGameManagerByUserChoice(ViewGame* v, Board& b, Player* p1, Player* p2, MoveLogic* ml) {
+	//create and return a player type according to user's choice
+	switch(choice_) {
+	//in both cases 1,2 we use a local game manager
+	case 1:
+	case 2:
+		return new LocalGameManager(v, b, p1, p2, ml);
+
+	case 3:
+		return new RemoteGameManager(v, b, p1, p2, ml);
+
+	default:
+		throw("Problem creating game manager");
+	}
 }
